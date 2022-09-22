bf3b721aa02bf3d16117b558a1b54316bc04905b
Raymond Ha <Haro@mail.uc.edu>
2022-09-22 15:09:48 -0400

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        double shippingCost;
+        int itemPrice;
+        double totalCost;
+
+        itemPrice = 100;
+        shippingCost= itemPrice * .02;
+        totalCost = itemPrice + shippingCost;
+
+        if(itemPrice >= 100)
+        {
+            System.out.println("The shipping cost of the item is free.");
+            System.out.println("The total cost is " + itemPrice);
+        }
+        else
+        {
+            System.out.println("The shipping cost of the item is " + shippingCost);
+            System.out.println("The total cost of the item is " + totalCost);
+        }
+
     }
 }
